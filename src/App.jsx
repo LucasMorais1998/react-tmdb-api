4b4c613bb5ae9b6826eb13946b6b4e3c3d30ee8c
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-07-31 17:04:01 -0300

    configure App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Link, Outlet } from 'react-router-dom';
+
 function App() {
   return (
     <div className='App'>
-      <h2>Movies Lib</h2>
+      <nav id='navbar'>
+        <Link to='/'>MoviesLib</Link>
+        <Link to='/movie/1'>Movie</Link>
+        <Link to='/search'>Search</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
